0d6620f38298d2008dbbbd3577f01b9480418343
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2021-10-16 18:43:05 +0300

    full solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import "./css/styles.css";
+import menu from "./menu.json";
+import templateRecipe from './templates/recipe.hbs';
 
-console.log("Мастерская: деплой билда от Parcel на GitHub Pages");
+const listMenu = document.querySelector('.js-menu');
+const markup = templateRecipe(menu);
+listMenu.insertAdjacentHTML('beforeend', markup);
+
+
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+console.log(checkboxRef);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+
+
+const onSwitchTheme = () => {
+    const currentTheme = localStorage.getItem('localStorageTheme') === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+    localStorage.setItem('localStorageTheme', currentTheme);
+    if (currentTheme === Theme.LIGHT) {
+        bodyRef.classList.add('light-theme');
+        bodyRef.classList.remove('dark-theme');
+    }
+    else {
+        bodyRef.classList.add('dark-theme');
+        bodyRef.classList.remove('light-theme');
+    };
+}
+
+
+checkboxRef.addEventListener('change', onSwitchTheme);
+
+const savedTheme = localStorage.getItem('localStorageTheme');
+switch (savedTheme) {
+    case Theme.LIGHT:
+        bodyRef.classList.add('light-theme');
+        break;
+    case Theme.DARK:
+        bodyRef.classList.add('dark-theme');
+        checkboxRef.setAttribute('checked', true);
+        break;
+    default:
+        bodyRef.classList.add('light-theme');
+}
+
+
